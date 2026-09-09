a84223667f074d9f14492d8c0a6976e197f6d870
jonah-b97 <bonischjonah@gmail.com>
2026-09-09 20:04:19 +0200

    0.0.2:Plazer implementation

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import pygame
+import pygame  # pyright: ignore[reportMissingImports]
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
-from constants import SCREEN_WIDTH,SCREEN_HEIGHT
+from player import Player
+
+
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -8,12 +12,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                     return
+        for item in updatable:
+            item.update(dt)
         screen.fill("black")
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         dt = clock.tick (60) / 1000
         print(f"{dt}")
